1d436cb467fc77b147458a8322e905c0b300fe5d
Felipe Augusto <felipeaugusto@MacBook-Air-de-Felipe.local>
2025-12-04 16:02:44 -0300

    att selectors

--- cypress/e2e/loginPage.cy.js
+++ cypress/e2e/loginPage.cy.js
@@ -11,15 +11,32 @@ describe('Orange HRM Tests', () => {
   sectionTitleTopBar: '.oxd-topbar-header-breadcrumb-module',
   dashboardGrid: '.orangehrm-dashboard-grid',
   wrongCredentialAlert: '.oxd-alert',
+  myInfoButton: "[href='/web/index.php/pim/viewMyDetails']",
+  firstNameField: '.orangehrm-firstname',
+  middleNameField: '.orangehrm-middlename',
+  lastNameField: '.orangehrm-lastname',
+  genericField: '.oxd-input',
+  dateField: "[placeholder='yyyy-dd-mm']",
+  closeDateButton: '.--close'
+  
+  
  }
 
-  it('Login Success', () => {
+  it.only('Login Success', () => {
     cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type('Admin')
     cy.get(selectorsList.passwordField).type('admin123')
     cy.get(selectorsList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
     cy.get(selectorsList.dashboardGrid)
+    cy.get(selectorsList.myInfoButton).click()
+    cy.get(selectorsList.firstNameField).type('Felipe')
+    cy.get(selectorsList.middleNameField).type('Augusto')
+    cy.get(selectorsList.lastNameField).type('Costa')
+    cy.get(selectorsList.genericField).eq(4).type('Employee Id Test')
+    cy.get(selectorsList.genericField).eq(5).type('other Id Number')
+    cy.get(selectorsList.genericField).eq(6).type('Driver License Number')
+    cy.get(selectorsList.dateField).eq(1).type('2025-09-09').type(selectorsList.closeDateButton)
 
   })
   
